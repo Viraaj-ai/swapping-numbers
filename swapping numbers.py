3c39c8898cea85a4540fa500f070a052fbcf82fe
Viraaj-ai <vintejvir@gmail.com>
2025-04-24 15:50:14 +0530

    swapping

---
+++ swapping numbers.py
@@ -0,0 +1,9 @@
+a=int(input("enter any number:"))
+b=int(input("enter any number:"))
+print("Value of variable a before swapping ",a)
+print("Value of variable b before swapping ",b)
+temp=a
+a=b
+b=temp
+print("Value of variable a after swapping ",a)
+print("Value of variable b after swapping ",b)
